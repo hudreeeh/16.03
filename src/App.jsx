a0d5770a748c01476da3c4bd65ad0062cee77cf7
hudreeh <sheridandamien7@gmail.com>
2022-03-16 11:36:22 +0600

    feat: ended 16.03

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Content from "./Content";
+
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Content/>
+      <Footer/>
+    </div>
+    
+  )
+}
+
+export default App;
+
+
